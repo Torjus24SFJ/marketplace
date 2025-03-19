50f39796bc55647ce1f05639133835543a7f4141
Torjus <Torjus24sfj@gmail.com>
2025-03-19 12:54:27 +0100

    DataContext provider

--- src/context/DataContext/DataContext.jsx
+++ src/context/DataContext/DataContext.jsx
@@ -1,6 +1,19 @@
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 
-fetch("https://fakestoreapi.com/products")
+// eslint-disable-next-line react-hooks/rules-of-hooks
+export const DataContent = useContext();
+
+export const DataProvider = ({ children }) => {
+  const [productList, setProductList] = useState([]);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
       .then((response) => response.json())
       .then((data) => console.log(data));
-  
+  }, []);
+
+  return (
+    <>
+    </>
+  )
+};
