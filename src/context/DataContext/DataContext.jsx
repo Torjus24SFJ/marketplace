642e4000fa5c4c9a6121c7572077a85dcb9f87d4
Torjus <Torjus24sfj@gmail.com>
2025-03-20 10:11:53 +0100

    Adding useState in DataContext for Selected categories, sets an array of categories with category object values & All for every product

--- src/context/DataContext/DataContext.jsx
+++ src/context/DataContext/DataContext.jsx
@@ -2,6 +2,13 @@ import { createContext, useState, useEffect } from "react";
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const ProductListContext = createContext();
+const [productList] = useState();
+const [selectedCategory, setSelectedCetegory] = useState(null);
+
+const categories = [
+  "All",
+  ...new Set(productList.map((product) => product.category)),
+];
 
 export const DataProvider = ({ children }) => {
   const [productList, setProductList] = useState([]);
@@ -11,14 +18,13 @@ export const DataProvider = ({ children }) => {
       .then((response) => response.json())
       .then((data) => {
         setProductList(data);
-        console.log(data); 
+        console.log(data);
       });
   }, []);
 
   return (
     <ProductListContext.Provider value={{ productList }}>
-        {children}
+      {children}
     </ProductListContext.Provider>
-  )
+  );
 };
-
